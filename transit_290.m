d951f7360f0fd8a5c4e5b660cf6b2549462b6d08
Shawn Swist <stb29@wildcats.unh.edu>
2018-11-07 13:29:20 -0800

    Frequency defined function for estimation

--- transit_290.m
+++ transit_290.m
@@ -8,15 +8,16 @@
 % Earth Parameters 
 rE = 6378;          % [km]       Earth radius
 mu = 3.986e5;       % [km^3/s^2] Earth gravational parameter
+seed = rng(1);      % Keep the seed the same for randn
 
 % Frequency of tag
-f0 = 100;           % [Hz]       Frequency
-c = 3e8;            % [m/s]      Speed of light
+f0 = 400;           % [MHz]      Frequency
+c = 299792458;      % [m/s]      Speed of light
 
 
 % Time
 epoch = 58398;      % [MJD] 10/7/2018
-tvec = linspace(0,1.5/24,500);
+tvec = linspace(.0215,.03,500); % narrowed down the time vector so more points are in visible section
 
 % Ground Station
 GClat = 37.426622;
@@ -30,7 +31,7 @@ GC_lam = GClong;
 a = rE + 500;   % [km]  Semi-major axis
 e = 0.01;       %  []   Eccentricity
 i = 89;         % [deg] Inclination
-Om = 90;         % [deg]    RAAN
+Om = 90;        % [deg]    RAAN
 w = 0;          % [deg] Argument of Periapsis
 nu = deg2rad(0);         % [deg->rad] True anomaly
 M = E2M(anom2E(nu,e),e);  % [rad] Mean anomaly
@@ -54,10 +55,11 @@ for jj = 1:length(r_enu)
 end
 
 
-% 'Vectorized' measurement function
+% Measurement function
 X = r_ecef(:,viz);
 V = v_ecef(:,viz);
 H = @(P) diag((X-P)'*V)./sqrt(diag((X-P)'*(X-P)));
+H2 = @(P) f0*(1+-(1000*H(P))/c);  % convert velocity from km/s to m/s
 
 
 % Numerical solution
@@ -69,22 +71,31 @@ drmag(end+1) = drmag(end); % repeat last data point (numerical derivatives lose 
 % let the numerical solution be the real data to use...
 % Cost function
 data = drmag;
-data = h(viz) + 0.1*randn(length(viz),1); % add noise, 0 mean 0.1 std
+data = h(viz) + 0.01*randn(length(viz),1); % add noise, 0 mean 0.01 std
 J = @(P) H(P) - data;
 
-% Nonlinear least squares solver
-pos = lsqnonlin(J,[rE;0;0]);
+% Nonlinear least squares solver - velocity based
+opts = optimoptions('lsqnonlin','OptimalityTolerance',1e-15,'Display','off');
+pos = lsqnonlin(J,[rE;0;0],[],[],opts);
     % seems to do decent with a bad guess. The numerical "true data" looks
     % like it makes it miss based on the residuals.
-
-
 Elat = asind(pos(3)/norm(pos));
 Elong = rad2deg(atan2(pos(2),pos(1)));
 
 
 
 % Doppler Shift
-freq = f0*(1+-(1000*drmag)/c);
+freq = f0*(1+-(1000*h(viz))/c);
+noise = 0.001*randn(length(h(viz)),1); % noise in kHz
+fdata = freq.*(1+noise*.001);          % noise to MHz
+
+J2 = @(P) H2(P) - fdata;
+
+% Nonlinear least squares solver - frequency based
+pos2 = lsqnonlin(J,[rE;0;0],[],[],opts);
+
+Elat2 = asind(pos2(3)/norm(pos2));
+Elong2 = rad2deg(atan2(pos2(2),pos2(1)));
 
 %% PLOTS
 % plot the estimated locataion vs true location
@@ -96,26 +107,27 @@ topoplot = [topo(:, 181:360), topo(:, 1:180)];
 contour(-180:179, -90:89, topoplot, [0, 0], 'black');
 [T] = plot(GClong, GClat, 'bs');
 [E] = plot(Elong,Elat,'rp');
-legend([T,E],'True Position','Estimated Position');
+[E2] = plot(Elong2,Elat2,'g^');
+legend([T,E,E2],'True Position','Estimated Position - Velocity','Estimated Position - Frequency');
 xlabel('Longitude'); ylabel('Latitude')
 title('Estimated Location vs. True Location')
 
 
 %% other plots
-figure;
-hold all
-earthPlot;
-plot3(r_ecef(1,viz),r_ecef(2,viz),r_ecef(3,viz))
-title('Satellite ECEF when Visible');
+% figure;
+% hold all
+% earthPlot;
+% plot3(r_ecef(1,viz),r_ecef(2,viz),r_ecef(3,viz))
+% title('Satellite ECEF when Visible');
 
 % figure;
 % plot3(r_enu(1,viz),r_enu(2,viz),r_enu(3,viz))
 % xlabel('E');ylabel('N');zlabel('U');
 % title('View of sat from GS');
 
-figure;
-plot(tvec*24,EL)
-title('Elevation angle of satellite');
+% figure;
+% plot(tvec*24,EL)
+% title('Elevation angle of satellite');
 
 figure;
 plot(tvec(viz)*24,drmag); hold on
@@ -125,10 +137,13 @@ plot(tvec(viz)*24,data,'k:')
 title('Relative Velocity of Satellite');
 legend('Numerical','h(p)','estimated pos','data used','location','northwest')
 
-figure;
-plot(tvec(viz)*24,h(viz)-drmag)
-ylabel('h(p) - numerical')
+% figure;
+% plot(tvec(viz)*24,h(viz)-drmag)
+% ylabel('h(p) - numerical')
 
 figure;
-plot(tvec(viz)*24,freq)
+plot(tvec(viz)*24,freq,tvec(viz)*24,H2(pos2),tvec(viz)*24,fdata)
 title('Frequency obseved by satellite');
+legend('True','Estimated','Data used')
+
+pos2-Rgs
